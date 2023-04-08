63def55253337e54de17b67003a9218480c2b6a7
hellbovh <69521565+hellbovh@users.noreply.github.com>
2023-04-08 12:05:37 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,42 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+ 	T* arr;
+	int top_index;
+ public:
+ 	TStack() : top_index(-1), arr(new T(size)) {}
+	bool isFull() const {
+		return top_index == size - 1;
+	}
+	bool isEmpty() const {
+		return top_index == -1;
+	}
+	void push(T value) {
+		if (isFull()) {
+			throw std::string("Stack is full!");
+		} else {
+			arr[++top_index] = value;
+		}
+	}
+	const T& pop() {
+		if (isEmpty()) {
+			throw std::string("Stack is empty!");
+		} else {
+			return arr[top_index--];
+		}
+	}
+	const T& get() const {
+		if (isEmpty()) {
+			throw std::string("Stack is empty!");
+		} else {
+			return arr[top_index];
+		}
+	}
 };
 
 #endif  // INCLUDE_TSTACK_H_
